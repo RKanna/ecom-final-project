00558a370460a1a0a1fca2c0dafe7ad1383825c7
RKanna <rkannan.balakrishnan@hotmail.com>
2023-12-01 08:25:39 +0530

    Shipping page to payment page Redirection success

--- src/screens/Shipping.page.jsx
+++ src/screens/Shipping.page.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { collection, addDoc } from "firebase/firestore/lite";
+import { Link, useNavigate } from "react-router-dom";
 import { appDB } from "../utils/firestore.js";
 const Shipping = () => {
+  const navigate = useNavigate();
   const [profile, setProfile] = useState({
+    name: "",
     address: "",
     city: "",
     pincode: "",
@@ -20,13 +23,17 @@ const Shipping = () => {
   const saveChange = async (e) => {
     e.preventDefault();
     try {
-      const res = await addDoc(collection(appDB, "user-Address"), {
+      const res = await addDoc(collection(appDB, "shipping-details"), {
         address: profile.address,
         city: profile.city,
         pincode: profile.pincode,
         country: profile.country,
       });
       alert("Data is successfully saved");
+      console.log(res);
+      if (res) {
+        navigate("/payment");
+      }
     } catch (err) {
       alert("Data not exported to Firestore");
       console.log(err.code);
@@ -37,6 +44,16 @@ const Shipping = () => {
     <section className="Shipping-page">
       <h1>Shipping Details</h1>
       <form className="shipping-form" onSubmit={saveChange}>
+        <div className="form-sub">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            placeholder="Enter your Name"
+            value={profile.name}
+            onChange={handleChange}
+          />
+        </div>
         <div className="form-sub">
           <label htmlFor="address">Address</label>
           <input
